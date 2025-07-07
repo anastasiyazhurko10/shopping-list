466d2b06dbb0465c7a9780349cab63818fca1d46
anastasiyazhurko10 <anastasiya.belanovich10@mail.ru>
2025-07-07 20:53:04 +0300

    Добавляю проверку с помощью условного оператора if
    Если значение переменной text равно пустой строке, выводится сообщение-предупреждение "Пожалуйста, введите текст" и останавливается с помощью return,  благодаря которому пустые элементы не добавляются

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const itemsContainer = document.getElementById('items');
 
 function addListItem() {
     const text = input.value.trim().toLowerCase();
+
+    if (text === '') {
+        alert("Пожалуйста, введите текст!");
+        return
+    }
 }
